749598deee14d84cf9156ef08b1392402f42c17b
Adam Rębowicz <arebowicz@gmail.com>
2022-12-01 19:56:43 +0100

    005 - pierwszy skrypt JS - lista zakupów

---
+++ 005 - pierwszy skrypt js/js/script.js
@@ -0,0 +1,26 @@
+let ul;
+let newItemForm;
+let shoppingList;
+
+document.addEventListener('DOMContentLoaded', () => {
+  ul = document.getElementById('shoppingList');
+  inputError = document.getElementById('inputError');
+  newItemForm = document.getElementById('newItemForm');
+  newItemForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if(event.target.elements[0].value.length > 2) {
+      addListItem(event.target.elements[0].value);
+      event.target.elements[0].classList.remove('inputDanger');
+      inputError.innerText = "";
+    } else {
+      inputError.innerText = "zle dane wejsciowe";
+      event.target.elements[0].classList.add('inputDanger');
+    }
+  });
+});
+
+function addListItem(shoppingItem) {
+  let li = document.createElement('li');
+  li.innerText = shoppingItem;
+  ul.appendChild(li);
+};
