fb0ee106b279c0577c724475cd09e49adcc66abe
Adam Rębowicz <arebowicz@gmail.com>
2022-12-12 17:48:45 +0100

    015 - printer i poprawki

--- 015 - zadanie rekrutacyjne/js/script.js
+++ 015 - zadanie rekrutacyjne/js/script.js
@@ -1,13 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   let showMoreText = document.querySelector(".show-more-action");
-  let showBox = document.querySelector(".show-more .courier-box");
+  let showMore = false;
   showMoreText.addEventListener("click", () => {
-    if (showMoreText.innerText === "pokaż więcej") {
+    if (!showMore) {
       showMoreText.innerHTML = `<h2>schowaj</h2>`;
-      showBox.style.display = "flex";
-    } else if (showMoreText.innerText === "schowaj") {
+      document.querySelector(".show-more .courier-box").style.display = "flex";
+      showMore = true;
+    } else {
       showMoreText.innerHTML = `<h2>pokaż więcej</h2>`;
-      showBox.style.display = "none";
+      document.querySelector(".show-more .courier-box").style.display = "none";
+      showMore = false;
+    }
+  });
+  let printToggler = document.querySelector(".print-toggler");
+  let printingOff = true;
+  printToggler.addEventListener("click", () => {
+    if (printingOff) {
+      printToggler.children[0].style.display = "flex";
+      printToggler.children[1].style.display = "none";
+      printingOff = false;
+    } else {
+      printToggler.children[0].style.display = "none";
+      printToggler.children[1].style.display = "flex";
+      printingOff = true;
     }
   });
 });
